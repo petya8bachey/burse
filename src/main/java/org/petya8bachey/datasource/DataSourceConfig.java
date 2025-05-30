cbac6be3021f5250b495dceb80e644c813cd2751
Peter <123090696+petya8bachey@users.noreply.github.com>
2025-05-30 20:11:24 +0300

    login

--- src/main/java/org/petya8bachey/datasource/DataSourceConfig.java
+++ src/main/java/org/petya8bachey/datasource/DataSourceConfig.java
@@ -11,44 +11,16 @@ import org.springframework.context.annotation.Primary;
 @Configuration
 public class DataSourceConfig {
 
-    // 1. Определяем и привязываем свойства для основного (admin) источника данных
     @Bean
     @Primary
-    @ConfigurationProperties("spring.datasource") // Привязывает свойства с префиксом 'spring.datasource'
-    public DataSourceProperties adminDataSourceProperties() {
+    @ConfigurationProperties("spring.datasource")
+    public DataSourceProperties appDataSourceProperties() {
         return new DataSourceProperties();
     }
 
-    // 2. Создаем основной HikariDataSource, используя привязанные свойства
     @Bean
     @Primary
-    public HikariDataSource adminDataSource(@Qualifier("adminDataSourceProperties") DataSourceProperties properties) {
-        return properties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
-    }
-
-    // 3. Определяем и привязываем свойства для клиентского источника данных
-    @Bean
-    @ConfigurationProperties("app.datasource.client") // Привязывает свойства с префиксом 'app.datasource.client'
-    public DataSourceProperties clientDataSourceProperties() {
-        return new DataSourceProperties();
-    }
-
-    // 4. Создаем клиентский HikariDataSource, используя привязанные свойства
-    @Bean
-    public HikariDataSource clientDataSource(@Qualifier("clientDataSourceProperties") DataSourceProperties properties) {
-        return properties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
-    }
-
-    // 5. Определяем и привязываем свойства для брокерского источника данных
-    @Bean
-    @ConfigurationProperties("app.datasource.broker") // Привязывает свойства с префиксом 'app.datasource.broker'
-    public DataSourceProperties brokerDataSourceProperties() {
-        return new DataSourceProperties();
-    }
-
-    // 6. Создаем брокерский HikariDataSource, используя привязанные свойства
-    @Bean
-    public HikariDataSource brokerDataSource(@Qualifier("brokerDataSourceProperties") DataSourceProperties properties) {
+    public HikariDataSource appDataSource(@Qualifier("appDataSourceProperties") DataSourceProperties properties) {
         return properties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
     }
 }
